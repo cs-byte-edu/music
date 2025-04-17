ff2809c2d112c5e068378dbad13289d832511ac0
Yugin <computer.science.byte@gamil.com>
2025-04-17 12:59:26 +0300

    Accessibility

--- config/tasks/images.js
+++ config/tasks/images.js
@@ -1,21 +1,21 @@
-import { src, dest } from 'gulp';
-import imagemin, { svgo, mozjpeg, optipng, gifsicle } from 'gulp-imagemin';
-import newer from 'gulp-newer';
-import _if from 'gulp-if';
-import { projectPath } from '../settings.js';
-import { handleError } from '../utils.js';
+import { src, dest } from "gulp";
+import imagemin, { svgo, mozjpeg, optipng, gifsicle } from "gulp-imagemin";
+import newer from "gulp-newer";
+import _if from "gulp-if";
+import { projectPath } from "../settings.js";
+import { handleError } from "../utils.js";
 
 export function processImages(isDev, serverInstance) {
   return () =>
     src(projectPath.src.img, { encoding: false })
-      .pipe(handleError('Images processing error...'))
+      .pipe(handleError("Images processing error..."))
       .pipe(newer(projectPath.dist.img))
       .pipe(
         _if(
           !isDev,
           imagemin([
-            svgo({ plugins: [{ name: 'removeViewBox', active: false }] }),
-            mozjpeg({ quality: 80, progressive: true }),
+            svgo({ plugins: [{ name: "removeViewBox", active: false }] }),
+            mozjpeg({ quality: 70, progressive: true }),
             optipng({ optimizationLevel: 3 }),
             gifsicle({ interlaced: true }),
           ])
